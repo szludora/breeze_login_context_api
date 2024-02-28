52b950432c56452a8d010499a040ef1a827612c8
szludora <szlucska.dora@gmail.com>
2024-02-28 18:49:11 +0100

    nav komponens, layout

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,24 +1,10 @@
 import { Outlet } from "react-router-dom";
-
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import MyNav from "../component/MyNav"
 
 export default function Layout() {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <MyNav/>
       <Outlet />
     </>
   );
