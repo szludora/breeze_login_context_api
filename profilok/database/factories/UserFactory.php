54ecf06d1d324147fe0aed44a67021eefc576419
szludora <szlucska.dora@gmail.com>
2024-02-28 18:12:03 +0100

    breeze install, setup

--- profilok/database/factories/UserFactory.php
+++ profilok/database/factories/UserFactory.php
@@ -24,10 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => fake('hu_HU')->name(),
+            'email' => fake('hu_HU')->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'jogosultsag' => rand(0, 3),
             'remember_token' => Str::random(10),
         ];
     }
