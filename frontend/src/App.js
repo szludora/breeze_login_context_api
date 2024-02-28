d3fe116d74d0177e94f93204a9895de4926a88c7
szludora <szlucska.dora@gmail.com>
2024-02-28 18:31:22 +0100

    alap route, bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./layout/Layout";
+import NoPage from "./pages/NoPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} /> */}
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/NoPage.js
@@ -0,0 +1,8 @@
+export default function NoPage() {
+  return (
+    <>
+      <div>Sorry, the page what you're searching for is not existing</div>
+      <div></div>
+    </>
+  );
+}
