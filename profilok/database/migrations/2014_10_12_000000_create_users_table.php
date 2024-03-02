be1cebfe4c28450f75e98f2a325283ec0c0148b1
szludora <szlucska.dora@gmail.com>
2024-03-02 14:14:18 +0100

    backend beáll

--- profilok/database/migrations/2014_10_12_000000_create_users_table.php
+++ profilok/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,10 +18,18 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('jogosultsag');
+            $table->tinyInteger('jogosultsag')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // tesztadat
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'jogosultsag' => 3,
+            'password' => 'admin'
+        ]);
     }
 
     /**
